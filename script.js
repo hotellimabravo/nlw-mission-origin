a8ab6fe0cb89f03f47d8fb22e4cdc3f3322eaeb9
Hugo Leonardo Baltazar <hugobalttazar@gmail.com>
2021-10-24 08:00:53 -0300

    alterando script e index

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const gitHub = "hotellimabravo";
-const youtube = "UCafkYu491JF-grSh1-0fUdA";
-const facebook = "hotellimabravo";
-const instagram = "hbalttazar";
-const twitter = "hotellimabravo";
+const linksSocialMedia = {
+    github: 'hotellimabravo',
+    youtube: 'UCafkYu491JF-grSh1-0fUdA',
+    facebook: 'hotellimabravo',
+    instagram: 'gabiambiental',
+    twitter: 'hotellimabravo'
+}
 
+function changeSocialMediaLinks() {
+    userName.textContent = "Hugo Baltazar";
+    for (let li of socialLinks.children) {
+       const social = li.getAttribute('class');
+       li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`;
 
+    }
+}
+changeSocialMediaLinks();
 
+function getGitHubProfileInfos() {
+    
+}
+
+getGitHubProfileInfos();
