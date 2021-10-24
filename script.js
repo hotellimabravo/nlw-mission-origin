71b3c305dc73222858b47bb3aa382d71cce48de9
Hugo Leonardo Baltazar <hugobalttazar@gmail.com>
2021-10-23 21:29:58 -0300

    adiciona script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const gitHub = "hotellimabravo";
+const youtube = "UCafkYu491JF-grSh1-0fUdA";
+const facebook = "hotellimabravo";
+const instagram = "hbalttazar";
+const twitter = "hotellimabravo";
+
+
+
