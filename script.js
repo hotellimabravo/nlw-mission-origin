4e9366080a42ec28047f4f4258f34d31267e86be
Hugo Leonardo Baltazar <hugobalttazar@gmail.com>
2021-10-24 09:27:02 -0300

    removido alert - codigo ok

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function changeSocialMediaLinks() {
     for (let li of socialLinks.children) {
        const social = li.getAttribute('class');
        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`;
-       alert(li.children[0].href)
     }
 }
 changeSocialMediaLinks();
