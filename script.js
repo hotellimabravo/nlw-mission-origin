7c33c2aaf71c8ddf54eb8d1070d8dac9226b7014
Hugo Leonardo Baltazar <hugobalttazar@gmail.com>
2021-10-24 08:37:54 -0300

    consumindo api do github

--- script.js
+++ script.js
@@ -11,13 +11,19 @@ function changeSocialMediaLinks() {
     for (let li of socialLinks.children) {
        const social = li.getAttribute('class');
        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`;
-
     }
 }
 changeSocialMediaLinks();
 
 function getGitHubProfileInfos() {
-    
-}
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
 
+    fetch(url).then(response => response.json()).then(data => {
+        userName.textContent = data.name;
+        gitUser.textContent = data.login;
+        userLink.href = data.html_url;
+        gitHubBio.textContent = data.bio;
+        avatarGit.src = data.avatar_url;
+    }) 
+}
 getGitHubProfileInfos();
